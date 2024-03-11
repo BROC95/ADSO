fe4d20e91c7b9a12d84bb6582bc24e96ca1b7ca6
Breyner Ocampo Cárdenas <43561128+BROC95@users.noreply.github.com>
2024-03-11 04:05:21 -0300

    Update questions.js
    Location path

--- backend/questions.js
+++ backend/questions.js
@@ -1,11 +1,10 @@
 let file = "./frontend/assets/questions.txt";
-// const location1 = window.location.pathname;
-// const directoryPath1 = location.substring(0, location.lastIndexOf("/") + 1);
-// if (directoryPath1 == "/" || directoryPath1 == "/ADSO/") {
-//   file = "./frontend/assets/questions.txt";
-// } else {
-//   file = "../assets/questions.txt";
-// }
+const location1 = window.location.pathname;
+const directoryPath1 = location1.substring(0, location1.lastIndexOf("/") + 1);
+if (directoryPath1 == "/" || directoryPath1 == "/ADSO/") {
+  file = "./frontend/assets/questions.txt";
+ } else {  file = "../assets/questions.txt";
+}
 let contenido = await fetch(file)
   .then((res) => res.text())
   .then((contenido) => {
